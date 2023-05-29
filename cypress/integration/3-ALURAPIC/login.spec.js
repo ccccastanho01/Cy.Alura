55047eb34a4a50ef73b6f075cf7577e2aebbca4a
Caio Castanho <ccccastanho@gmail.com>
2023-05-29 02:30:36 -0300

    Ajustes dos Testes

--- cypress/integration/3-ALURAPIC/login.spec.js
+++ cypress/integration/3-ALURAPIC/login.spec.js
@@ -14,23 +14,3 @@ expect(str).to.equal('Invalid user name or password')
 })
 })
 })
-/* describe('Login de ususarios alura pic', () => {
-
-    beforeEach(() => {
-        cy.visit('/');
-        cy.intercept('POST', 'https://apialurapic.herokuapp.com/user/login', { statusCode: 400 }).as('stubPost')
-    })
-
-    it.only('Login válido', () => {
-        cy.login(Cypress.env('username'), Cypress.env('password'));
-        cy.wait('@stubPost')
-        cy.contains('a', '(Logout)').should('be.visible');
-    })
-
-    it('Login inválido', () => {
-        cy.login('Gertrudes', '1267');
-        cy.on('window:alert', (str) => {
-            expect(str).to.equal('Invalid user name or password');
-        });
-    });
-}) */
